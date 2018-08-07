1a3cc73c3b941e04ee303071a72f4096eff89d04
Silas Owings <silasowings@gmail.com>
2018-08-07 13:11:27 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+const notes = require('./notes.js');
+
+const titleOps = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+};
+const bodyOps = {
+  describe: 'Body text of the note',
+  demand: true,
+  alias: 'b'
+};
+
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOps,
+    body: bodyOps
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note',{
+    title: titleOps
+  })
+  .command('remove', 'Delete a note',{
+    title: titleOps
+  })
+  .help()
+  .argv;
+
+var command = argv._[0];
+
+
+if (command === 'add') {
+  var note = notes.addNote(argv.title, argv.body);
+  if(note){
+    console.log("Note added succesfully");
+    notes.logNote(note);
+  } else {
+    console.log("Duplicate note, process aborted");
+  }
+} else if (command === 'list') {
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note));
+} else if (command === 'read') {
+  var note = notes.getNote(argv.title);
+  if(note){
+    console.log("Note located");
+    notes.logNote(note);
+  } else {
+    console.log("Note not found");
+  }
+} else if (command === 'remove') {
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note was removed' : 'Note not found';
+  console.log(message);
+} else {
+  console.log('Command not recognized');
+}
